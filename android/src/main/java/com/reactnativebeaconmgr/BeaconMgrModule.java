5d1847a10a55c714440396906ee4052cebf35988
“Sam <sam.joseph@indysoft.com>
2021-06-22 15:09:52 -0400

    implement beacon methods

--- android/src/main/java/com/reactnativebeaconmgr/BeaconMgrModule.java
+++ android/src/main/java/com/reactnativebeaconmgr/BeaconMgrModule.java
@@ -1,19 +1,43 @@
 package com.reactnativebeaconmgr;
 
+import android.util.Log;
+import android.content.Context;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.service.ArmaRssiFilter;
+import org.altbeacon.beacon.service.RunningAverageRssiFilter;
+
 
 @ReactModule(name = BeaconMgrModule.NAME)
-public class BeaconMgrModule extends ReactContextBaseJavaModule {
+public class BeaconMgrModule extends ReactContextBaseJavaModule implements BeaconConsumer{
     public static final String NAME = "BeaconMgr";
+    private BeaconManager mBeaconManager;
+    private Context mApplicationContext;
+    private ReactApplicationContext mReactContext;
 
     public BeaconMgrModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        Log.d(NAME, "BeaconsAndroidModule - started");
+        this.mReactContext = reactContext;
     }
 
     @Override
@@ -22,7 +46,15 @@ public class BeaconMgrModule extends ReactContextBaseJavaModule {
         return NAME;
     }
 
+    private void sendEvent(String eventName, @Nullable WritableMap params) {
+        if (mReactContext.hasActiveCatalystInstance()) {
+            mReactContext
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit(eventName, params);
+        }
+    }
 
+   
     // Example method
     // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
@@ -31,4 +63,18 @@ public class BeaconMgrModule extends ReactContextBaseJavaModule {
     }
 
     public static native int nativeMultiply(int a, int b);
+
+    @Override
+    public void initialize() {
+        this.mApplicationContext = this.mReactContext.getApplicationContext();
+        this.mBeaconManager = BeaconManager.getInstanceForApplication(mApplicationContext);
+        // need to bind at instantiation so that service loads (to test more)
+        mBeaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:0-3=4c000215,i:4-19,i:20-21,i:22-23,p:24-24"));
+    }
+
+    @ReactMethod
+    public void setHardwareEqualityEnforced(Boolean e) {
+        Beacon.setHardwareEqualityEnforced(e.booleanValue());
+    }
+
 }
