7767e4a3adb2e0b85f44c07c242bf9daddf49505
“Sam <sam.joseph@indysoft.com>
2021-06-24 14:08:16 -0400

    update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, DeviceEventEmitter } from 'react-native';
 import BeaconMgr from 'react-native-beacon-mgr';
 
 export default function App() {
   const [result] = React.useState<number | undefined>();
 
+  const onSuccess = () => {
+    console.log('add');
+    return;
+  };
+
+  const onFail = () => {
+    console.log('fail');
+    return;
+  };
+
   React.useEffect(() => {
     BeaconMgr.setup();
+    BeaconMgr.addEstimotes(onSuccess, onFail);
   }, []);
+
+  React.useEffect(() => {
+    BeaconMgr.startRanging(
+      'REGION1',
+      '42a0e5cc-12bf-4ae9-be4e-2dbea495fc9b',
+      onSuccess,
+      onFail
+    );
+  }, []);
+
+  DeviceEventEmitter.addListener('beaconsDidRange', (data) => {
+    console.log('Found beacons!', data.beacons);
+  });
 
   return (
     <View style={styles.container}>
